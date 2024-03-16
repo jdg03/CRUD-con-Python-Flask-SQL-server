059799045c47d0da126c1ef8f23e09f1c2454099
jdg03 <gonzalez5.jose52@gmail.com>
2024-03-15 21:42:44 -0600

    .

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 PORT = 4000
-DB_USER = 'root'  # Nombre de usuario de la base de datos
-DB_PASSWORD = '1234'  # Contraseña de la base de datos
-DB_SERVER = 'DESKTOP-DN8TPKA'  # Nombre del servidor de la base de datos solo con localhost me funciono
+DB_USER = ''  # Nombre de usuario de la base de datos
+DB_PASSWORD = ''  # Contraseña de la base de datos
+DB_SERVER = ''  # Nombre del servidor de la base de datos solo con localhost me funciono
 DB_DATABASE = 'tareas'  # Nombre de la base de datos
 
 
